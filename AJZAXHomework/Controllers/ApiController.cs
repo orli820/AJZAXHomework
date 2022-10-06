f7f6de7c18d55874d4ce2efdb9a49bdc5f2c2903
苡錚 陳 <yikishow@gmail.com>
2022-10-06 16:47:09 +0800

    加入專案檔案。

---
+++ AJZAXHomework/Controllers/ApiController.cs
@@ -0,0 +1,31 @@
+﻿using AJZAXHomework.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJZAXHomework.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly DemoContext _context;
+
+        public ApiController (DemoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult checkname(string name)
+        {
+            var exists = _context.Members.Any(n => n.Name == name);
+            return Content(exists.ToString(), "text/plain");
+        }
+       
+    }
+}
---
+++ AJZAXHomework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AJZAXHomework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
